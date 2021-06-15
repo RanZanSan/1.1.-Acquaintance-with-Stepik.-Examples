445142bb020c813de2735029af5c724f68353082
Andrey Rasskazov <ranzansan@gmail.com>
2021-06-15 12:28:37 +0300

    Добавьте файлы проекта.

---
+++ Examples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // считываем две строки
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            /* выводим их объединение */
+            Console.WriteLine(str1 + str2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExamplesToo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamplesToo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // считываем переданное число
+            int a = int.Parse(Console.ReadLine());
+            // выводим сумму числа и единицы
+            Console.WriteLine(a + 1);
+        }
+    }
+}
